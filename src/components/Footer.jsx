142283f90cd811d1dc3d984c1839c449caa18215
Nimra-anjum <nimraanjum456@gmail.com>
2024-07-22 00:17:15 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.youtube.com/@nimraanjum3735" target={'blank'}>
+            Youtube
+          </a>
+          <a href="https://instagram.com/nxanjum" target={'blank'}>
+            Instagram
+          </a>
+          <a href="https://github.com/nimra-anjum" target={'blank'}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
